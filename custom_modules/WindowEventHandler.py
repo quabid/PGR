cf0817ed84e270d9f6d498fe954b3b92f9aa57be
quobod <quobod@gmail.com>
2021-07-18 19:39:56 -0400

    Refined WindowEventHandler module code

--- custom_modules/WindowEventHandler.py
+++ custom_modules/WindowEventHandler.py
@@ -1,37 +1,26 @@
 from .CoordinatesManager import save_location, get_location
 
 
-def on_open(content):
-    win_coordinates = get_location()
-    x = None
-    y = None
-    if win_coordinates:
-        x = win_coordinates['x']
-        y = win_coordinates['y']
-        content.geometry("+{}+{}".format(x, y))
-
-
-
-
-def config_location(content):
-
-    coordinates = {
-        'x': content.winfo_rootx(),
-        'y': content.winfo_rooty()
-    }
-    save_location(coordinates)
-    content.destroy()
-
-
 def window_handler(window):
 
     def on_close():
+        print("\n\n\t\tClosing the main window\n\n")
         coordinates = {
             'x': window.winfo_rootx(),
             'y': window.winfo_rooty()
         }
         save_location(coordinates)
         window.destroy()
+
+    def on_open(content):
+        print("\n\n\t\tOpening the main window")
+        win_coordinates = get_location()
+        x = None
+        y = None
+        if win_coordinates:
+            x = win_coordinates['x']
+            y = win_coordinates['y']
+            content.geometry("+{}+{}".format(x, y))
 
     window.protocol("WM_DELETE_WINDOW", on_close)
     on_open(window)
